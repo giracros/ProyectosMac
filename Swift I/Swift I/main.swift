53af8b5fc0ef2484ae1c7be79e6652844c9cdc1b
danielfuentes <giracros@gmail.com>
2015-06-01 22:40:34 -0500

    Swift I
    Tortuguitas I

---
+++ Swift I/Swift I/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Swift I
+//
+//  Created by Daniel Fuentes on 6/1/15.
+//  Copyright (c) 2015 Daniel Fuentes. All rights reserved.
+//
+
+//Importa componentes de Foundation
+import Foundation
+//Constantes
+let CONSTANTE = 5
+//Variables
+var numeroDecimal:Double
+var numeroEntero:Int
+//Asignacion
+numeroDecimal = 99.9
+numeroEntero = 5
+//Operaciones
+numeroEntero = numeroEntero + CONSTANTE
+//Resultado
+println("Numero Constante: " + CONSTANTE.description)
+println("Numero Decimal: " + numeroDecimal.description)
+println("Numero Entero: " + numeroEntero.description)
+
+

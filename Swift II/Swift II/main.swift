fa5875f8c6e25c990458ec280a54ab2086ec4165
danielfuentes <giracros@gmail.com>
2015-06-01 22:59:42 -0500

    Swift II
    Tortuguitas II

--- Swift II/Swift II/main.swift
+++ Swift II/Swift II/main.swift
@@ -1,6 +1,6 @@
 //
 //  main.swift
-//  Swift I
+//  Swift II
 //
 //  Created by Daniel Fuentes on 6/1/15.
 //  Copyright (c) 2015 Daniel Fuentes. All rights reserved.
@@ -18,7 +18,27 @@ numeroDecimal = 99.9
 numeroEntero = 5
 //Operaciones
 numeroEntero = numeroEntero + CONSTANTE
-//Resultado
+//Condicionales
+
+if numeroEntero < 12{
+
+    println("Hola Mundo blablablablablabl")
+
+}
+
+else
+
+{
+
+
+    println("Chao")
+}
+
+
+
+//Resultado se puede mostrar de dos maneras distintas, la diferencia es el numero de caracteres que usan 
+// Primera forma
 println("Numero Constante: " + CONSTANTE.description)
-println("Numero Decimal: " + numeroDecimal.description)
-println("Numero Entero: " + numeroEntero.description)
+// Segunda Forma
+println("Numero Decimal: \(numeroDecimal)")
+println("Numero Entero: \(numeroEntero.description)")
